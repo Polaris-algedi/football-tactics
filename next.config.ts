fd9c7b38ac342cc221ac7215a3f57b32d2bdfc3d
Polaris-algedi <karchalmohammed@gmail.com>
2026-06-11 19:10:44 +0100

    feat: Update images remote patterns to my specific bucket and placeholder images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "evotqfydmmastmhizxkz.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/player-images/**", // Locks it down to my specific bucket
+      },
+      // For placeholder images
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
